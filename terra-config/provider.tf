cad42d3cd8b349c0e812bcb0cd70c3b5624ad999
ThaiNguyen86 <ngtthai0604@gmail.com>
2025-11-13 22:37:06 +0700

    Refactor S3 backend configuration in provider.tf

--- terra-config/provider.tf
+++ terra-config/provider.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.2"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,12 +6,10 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "tfstate-584968901462-ap-southeast-1"
-    key    = "prod/terraform.tfstate"
-    region = "ap-southeast-1"
-    encrypt = true
-  }
+  required_version = ">= 1.2"
+
+    backend "s3" {}
+
 }
 
 provider "aws" {
